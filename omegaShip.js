e124ad483bac5c3ff562c8c07032a128c881d695
MatthewNguyenAYJ <42983707+MatthewNguyenAYJ@users.noreply.github.com>
2019-01-13 15:00:30 -0500

    moving ship file for omega

---
+++ omegaShip.js
@@ -0,0 +1,13 @@
+function omegaShip(){
+
+this.x = width/2;
+
+  this.show = function(){
+  var x = constrain(this.x, 10,775);
+    image(omega, x, height- 90, 70, 90)
+  }
+
+this.move = function(dir){
+  this.x += dir*10
+  }
+}
